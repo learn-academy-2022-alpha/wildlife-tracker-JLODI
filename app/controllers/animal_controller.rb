542a16901e056178c88c5f74fdc92fb5d261f510
Jennice <jennicelodi@gmail.com>
2022-03-25 11:50:46 -0700

    setup update, create, delete, show for animal

--- app/controllers/animal_controller.rb
+++ app/controllers/animal_controller.rb
@@ -3,4 +3,36 @@ class AnimalController < ApplicationController
         animals = Animal.all
         render json: animals
     end
+    def show
+        one_animal = Animal.find(params[:id])
+        render json: one_animal
+    end
+    def create
+        new_animal = Animal.create(animal_params)
+        if new_animal.valid?
+            render json: new_animal
+        else
+            render json: new_animal.errors
+        end
+    end
+    def update
+        one_animal = Animal.find(params[:id])
+        one_animal.update(animal_params)
+        if one_animal.valid?
+            render json: one_animal
+        else
+            render json: one_animal.errors
+        end
+    end
+    def destroy
+        one_animal = Animal.find(params[:id])
+        if one_animal.destroy
+            render json: one_animal
+        else
+            render json: one_animal.errors
+        end
+    end
+    def animal_params
+        params.require(:animal).permit(:common_name, :latin_name, :kingdom)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get 'animals' => 'animal#index'
+  get 'animals/:id' => 'animal#show'
+  post 'animals' => 'animal#create'
+  patch 'animals/:id', to: 'animal#update'
+  delete 'animals/:id(.:format)' => 'animal#destroy'
 end
